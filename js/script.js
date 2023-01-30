82faf7ebdac7b849487afeef605174c0a33651db
luhaspede <luhaspede@gmail.com>
2023-01-30 19:41:04 -0300

    Btn read_more con transition

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
 
-// Función botón leer mas
-let hideText_btn = document.getElementById('hideText_btn');
-let hideText = document.getElementById('hideText');
+// Función botón ver mas
+let hiddenText_btn = document.getElementById('hiddenText_btn');
+let hiddenText = document.getElementById('hiddenText');
 
-hideText_btn.addEventListener('click',toggleText);
+hiddenText_btn.addEventListener('click',toggleText);
+
+// function toggleText(){
+// 	hiddenText.classList.toggle('text-show');
+// 	if (hiddenText.classList.contains('text-show')) {
+// 		br_activo.innerHTML='<br>';
+// 		hiddenText_btn.innerHTML = 'Ver menos';
+// 	}
+// 	else{
+// 	hiddenText_btn.innerHTML='Ver mas';
+// 	br_activo.innerHTML='';
+// 	}
+// }
 
 function toggleText(){
-	hideText.classList.toggle('text-show');
-	if (hideText.classList.contains('text-show')) {
+	if (hiddenText.classList.contains('text-hidden')) {
 		br_activo.innerHTML='<br>';
-		hideText_btn.innerHTML = 'Ver menos';
-	}
-	else{
-	hideText_btn.innerHTML='Leer mas';
+		hiddenText.classList.remove('text-hidden');
+		hiddenText.classList.add('text-show');
+		hiddenText_btn.innerHTML = 'Ver menos';
+	}else{
+	hiddenText_btn.innerHTML='Ver mas';
 	br_activo.innerHTML='';
-	}
-}
+	hiddenText.classList.remove('text-show');
+	hiddenText.classList.add('text-hidden');
+	};
+};
 
-// Funcion botón hacia arriba
+// Función botón hacia arriba
 const btn_scrolltop = document.getElementById('btn_scrolltop')
 btn_scrolltop.addEventListener('click', () =>{
 	window.scrollTo(0,0) 
 });
 
 window.onscroll = () => {
-	console.log(window.scrollY)
+	// console.log(window.scrollY)
 	if(window.scrollY > 100){
 		btn_scrolltop.classList.add("btn-scrolltop-on")
 	}else{
